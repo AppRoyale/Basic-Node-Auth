41c4e060aa102819a5a2373d0344ac23a11f4e8b
AppRoyale <schiemann.andre@gmx.de>
2014-02-01 20:56:32 +0100

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,19 +4,19 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' 		: '178348945693166', // your App ID
-		'clientSecret' 	: '0154d56569faf7060d69f657f4975ade', // your App Secret
+		'clientID' 		: '123', // your App ID
+		'clientSecret' 	: '123', // your App Secret
 		'callbackURL' 	: 'http://localhost:1337/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
-		'consumerKey' 		: 'your-consumer-key-here',
-		'consumerSecret' 	: 'your-client-secret-here',
+		'consumerKey' 		: '123',
+		'consumerSecret' 	: '123',
 		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
-		'clientID' 		: 'your-secret-clientID-here',
+		'clientID' 		: '123',
 		'clientSecret' 	: 'your-client-secret-here',
 		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
 	}
